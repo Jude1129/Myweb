be8c8d719a6c98e5dcf9dd2b84166625f9264f79
DVP <judejjacob3@gmail.com>
2025-07-24 23:01:34 +0530

    Updated full designs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function changeText() {
   document.getElementById('dynamicText').textContent =
-    "You clicked the button! Looks better now, right?";
+    "You clicked the button! JavaScript is working!";
 }
